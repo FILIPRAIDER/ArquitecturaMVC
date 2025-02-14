7a3efd376892a6518ecbad2765215dd85f2e58d2
FILIPRAIDER <filipraider123@gmail.com>
2025-02-14 16:00:09 -0500

    Correccion sql

--- modelo/ProductModel.js
+++ modelo/ProductModel.js
@@ -33,17 +33,17 @@ export const testDBConnection = async () => {
 
 const ProductModel = {
   async addProduct(nombre, precio) {
-    const sql = "INSERT INTO producto (nombre, precio) VALUES (?, ?)";
+    const sql = "INSERT INTO bzkm2uiwicqdkydr79t1 (nombre, precio) VALUES (?, ?)";
     await db.execute(sql, [nombre, precio]);
   },
 
   async listProducts() {
-    const [rows] = await db.execute("SELECT * FROM producto");
+    const [rows] = await db.execute("SELECT * FROM bzkm2uiwicqdkydr79t1");
     return rows;
   },
 
   async deleteProduct(id) {
-    const [result] = await db.execute("DELETE FROM producto WHERE id = ?", [id]);
+    const [result] = await db.execute("DELETE FROM bzkm2uiwicqdkydr79t1 WHERE id = ?", [id]);
     return result.affectedRows;
   },
 };

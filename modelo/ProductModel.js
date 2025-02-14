f58927f021936f7e91819d8873fda11ee1f36e09
FILIPRAIDER <filipraider123@gmail.com>
2025-02-14 16:02:47 -0500

    Correccion

--- modelo/ProductModel.js
+++ modelo/ProductModel.js
@@ -5,19 +5,19 @@ dotenv.config();
 
 
 const db = await mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "datos",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 export const testDBConnection = async () => {
   try {
     const connection = await mysql.createConnection({
-      host: process.env.DB_HOST || "localhost",
-      user: process.env.DB_USER || "root",
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME || "productdb",
+      database: process.env.DB_NAME,
     });
 
     await connection.ping(); // Verifica que la conexión es válida
@@ -33,17 +33,17 @@ export const testDBConnection = async () => {
 
 const ProductModel = {
   async addProduct(nombre, precio) {
-    const sql = "INSERT INTO bzkm2uiwicqdkydr79t1 (nombre, precio) VALUES (?, ?)";
+    const sql = "INSERT INTO producto (nombre, precio) VALUES (?, ?)";
     await db.execute(sql, [nombre, precio]);
   },
 
   async listProducts() {
-    const [rows] = await db.execute("SELECT * FROM bzkm2uiwicqdkydr79t1");
+    const [rows] = await db.execute("SELECT * FROM producto");
     return rows;
   },
 
   async deleteProduct(id) {
-    const [result] = await db.execute("DELETE FROM bzkm2uiwicqdkydr79t1 WHERE id = ?", [id]);
+    const [result] = await db.execute("DELETE FROM producto WHERE id = ?", [id]);
     return result.affectedRows;
   },
 };
